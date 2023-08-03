a6992bd791d1164937d1fba693a88c6cb7158aef
Abner <abnerferreira84@gmail.com>
2023-08-03 15:05:24 -0300

    refactor: Refatorando para usar CSS modules.

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image';
-import './Header.scss';
+import styles from './header.module.css';
 
 const Header = () => {
   return (
-      <header className="header-container">
-        <section className="header-logo">
+      <header className={styles.headerContainer}>
+        <section className={styles.headerLogo}>
           <Image
             width={176}
             height={50}
@@ -14,7 +14,7 @@ const Header = () => {
           />
         </section>
 
-        <section className="header-social-media">
+        <section className={styles.headerSocial}>
           <Image
             width={24}
             height={24}
