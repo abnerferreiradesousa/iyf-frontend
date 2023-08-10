36ed0a5c0894c41893ac005e7bd92a8f63062603
Rafael Santos <rafaelcs_94@hotmail.com>
2023-08-10 11:38:00 -0300

    feat: :lipstick: adds onhover effect on nav texts

--- src/app/components/AsideNavigation/AsideNavigation.tsx
+++ src/app/components/AsideNavigation/AsideNavigation.tsx
@@ -7,6 +7,10 @@ import styles from './asidenavigation.module.css';
 
 const AsideNavigation = ({ color }: { color: string }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isHover1, setIsHover1] = useState(false);
+  const [isHover2, setIsHover2] = useState(false);
+  const [isHover3, setIsHover3] = useState(false);
+  const [isHover4, setIsHover4] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -21,6 +25,7 @@ const AsideNavigation = ({ color }: { color: string }) => {
         className={styles.iconMenu}
         alt="Logo do International Youth Fellowship"
         onClick={toggleMenu}
+        style={{ width: '35%', height: '39%' }}
       />
 
       <section
@@ -37,16 +42,52 @@ const AsideNavigation = ({ color }: { color: string }) => {
         </section>
 
         <section className={styles.navigationMenu}>
-          <Link style={{ color: color }} href="#" className={styles.navItem}>
+          <Link
+            style={{
+              color: isHover1 ? '#fff' : color,
+              backgroundColor: isHover1 ? color : '',
+            }}
+            onMouseEnter={() => setIsHover1(!isHover1)}
+            onMouseLeave={() => setIsHover1(!isHover1)}
+            href="#"
+            className={styles.navItem}
+          >
             Institucional
           </Link>
-          <Link style={{ color: color }} href="#" className={styles.navItem}>
+          <Link
+            style={{
+              color: isHover2 ? '#fff' : color,
+              backgroundColor: isHover2 ? color : '',
+            }}
+            onMouseEnter={() => setIsHover2(!isHover2)}
+            onMouseLeave={() => setIsHover2(!isHover2)}
+            href="#"
+            className={styles.navItem}
+          >
             Cultura
           </Link>
-          <Link style={{ color: color }} href="#" className={styles.navItem}>
+          <Link
+            style={{
+              color: isHover3 ? '#fff' : color,
+              backgroundColor: isHover3 ? color : '',
+            }}
+            onMouseEnter={() => setIsHover3(!isHover3)}
+            onMouseLeave={() => setIsHover3(!isHover3)}
+            href="#"
+            className={styles.navItem}
+          >
             Educação
           </Link>
-          <Link style={{ color: color }} href="#" className={styles.navItem}>
+          <Link
+            style={{
+              color: isHover4 ? '#fff' : color,
+              backgroundColor: isHover4 ? color : '',
+            }}
+            onMouseEnter={() => setIsHover4(!isHover4)}
+            onMouseLeave={() => setIsHover4(!isHover4)}
+            href="#"
+            className={styles.navItem}
+          >
             Social
           </Link>
         </section>
